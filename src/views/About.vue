c12e8a1e5d6b844f41878424b7c2ec5e56156be7
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-07-10 10:57:05 +0300

    about page changed

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,9 +3,24 @@
     <div class="row">
       <div class="col mb-3">
         <div id="card-about" class="card mb-3">
-          <div class="card-body text-center">This site coded with Vue.js.</div>
+          <div class="card-body">
+            <vue-typed-js
+              :strings="['Vue.js.', 'Bootstrap.', 'Vue Typed.', 'Fontawesome Icons.' ]"
+              :loop="true"
+              :showCursor="true"
+              :smartBackspace="true"
+              :backSpeed="50"
+            >
+              <span>
+                This site coded with
+                <span class="typing"></span>
+              </span>
+            </vue-typed-js>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<style >
+</style>
